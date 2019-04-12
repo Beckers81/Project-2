0e2133e8c49714d79844ab007018a94db21c124e
becky henry <becky8135@gmail.com>
2019-04-11 19:25:00 -0600

    Latest code from Becky

--- models/bootcamp.js
+++ models/bootcamp.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Bootcamp = sequelize.define("Bootcamp", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Bootcamp;
 };
