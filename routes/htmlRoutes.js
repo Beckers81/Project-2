3c30802f2630d8e5bbbb2dee781bb02db9926759
bbanderson84 <bbanderson84@gmail.com>
2019-04-11 20:47:45 -0600

    Revert to 10dd396826b0cd056f3f7071c000b962a2195f2e

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,30 +1,6 @@
-var isAuthenticated = require("../config/middleware/isAuthenticated");
-
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     res.sendFile(process.cwd() + "/public/home.html");
-  });
-};
-
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    if (req.user) {
-      res.redirect("/activeuser");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-  app.get("/login", function(req, res) {
-    if (req.user) {
-      res.redirect("/activeuser");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  // isAuthenticated middleware to this route
-  app.get("/activeuser", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/activeuser.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var session = require("express-session");
-
-// Requiring Passport
-var passport = require("./config/passport");
 
 var bodyParser = require("body-parser");
 
@@ -16,12 +12,6 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
-// We need to use sessions to keep track of our user's login status
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
-app.use(passport.initialize());
 
 // Routes
 require("./routes/apiRoutes")(app);
