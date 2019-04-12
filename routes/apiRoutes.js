7245c9d27c7c9f99ea670f187e2610d5818fd7e2
Gtzquierdo <gtzquierdo@gmail.com>
2019-04-11 22:48:40 -0600

    lets cross our fingers

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
-// var db = require("../models");
+db = require("../models/");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
   // Get all examples
@@ -33,7 +34,7 @@ var courses = require("../data/courses.js");
 
 module.exports = function(app) {
   // when the url is "/api/table" display the data in json format
-  app.get("/api/courses", function(req, res) {
+  app.get("/api/courses", function(_req, res) {
     res.json(courses);
   });
 
@@ -88,5 +89,42 @@ module.exports = function(app) {
 
     // return the best match friend
     res.json(bestMatch);
+  });
+};
+
+module.exports = function(app) {
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json("/activeuser");
+  });
+  app.post("/api/signup", function(req, res) {
+    console.log(req.body);
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.json(err);
+      });
+  });
+  // Logging out User
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      res.json({});
+    } else {
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
   });
 };
