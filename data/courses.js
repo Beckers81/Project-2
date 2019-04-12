f45fe2f1bc3aa41c4686b230b5c8cb73a698769b
Bindhu <biji_100@yahoo.com>
2019-04-11 21:57:51 -0600

    Bindhu Changes

--- data/courses.js
+++ data/courses.js
@@ -13,7 +13,7 @@ var courses = [
       "http://alfa-trening.ru/wp-content/uploads/2014/08/zhenshhina-i-tsvetyi-2.jpg",
     scores: [3, 5, 4, 3, 1, 4, 5, 3, 4, 5],
     card:
-      "<div class='card-body'><h5 class='card-title'>Thinkful</h5><h6 class='card-subtitle mb-2 text-muted'><img src='./images/thinkful.png' alt='schoollogo'></h6><p class='card-text'>Phone: (858) 367-3232</p><a href='https://www.thinkful.com/?utm_campaign=search_national-jg_brand_q2.19&utm_medium=cpc&utm_content=thinkful-exact_c_homepage_no-query_340878314077&utm_term=thinkful_e_aud-381204705548%3Akwd-303219710301&utm_source=google&gclid=Cj0KCQjwnKHlBRDLARIsAMtMHDFlVKFpUebUID9aDnF4Z0rwN9vBoBEeuTz8DuCyQm7-ep-9_06dnAEaAiG0EALw_wcB'target='_blank' class='card-link>Learn More</a></div>"
+      "<div class='card-body'><h2 class='card-title'>Thinkful</h2><h6 class='card-subtitle mb-2 text-muted'><img src='./images/thinkful.png' alt='schoollogo'></h6><p class='card-text'>Phone: (858) 367-3232</p><a href='https://www.thinkful.com/?utm_campaign=search_national-jg_brand_q2.19&utm_medium=cpc&utm_content=thinkful-exact_c_homepage_no-query_340878314077&utm_term=thinkful_e_aud-381204705548%3Akwd-303219710301&utm_source=google&gclid=Cj0KCQjwnKHlBRDLARIsAMtMHDFlVKFpUebUID9aDnF4Z0rwN9vBoBEeuTz8DuCyQm7-ep-9_06dnAEaAiG0EALw_wcB'target='_blank' class='card-link>Learn More</a></div>"
   },
   {
     name: "GALVANIZE",
@@ -21,7 +21,7 @@ var courses = [
       "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
     scores: [1, 3, 4, 5, 1, 6, 2, 5, 1, 1],
     card:
-      "<div class='card-body'><h5 class='card-title'>Galvanize</h5><h6 class='card-subtitle mb-2 text-muted'><img src='./images/galvanize.png' alt='schoollogo'></h6><p class='card-text'>Phone: (720) 627-6862</p><a href='https://www.galvanize.com/courses/request-info' target='_blank' class='card-link'>LearnMore</a></div>"
+      "<div class='card-body'><h2 class='card-title'>Galvanize</h2><h6 class='card-subtitle mb-2 text-muted'><img src='./images/galvanize.png' alt='schoollogo'></h6><p class='card-text'>Phone: (720) 627-6862</p><a href='https://www.galvanize.com/courses/request-info' target='_blank' class='card-link'>Learn More</a></div>"
   },
   {
     name: "GENERAL ASSEMBLY",
@@ -29,7 +29,7 @@ var courses = [
       "http://1.bp.blogspot.com/-dNvgygUycDQ/Tq8p4SUphpI/AAAAAAAABjo/UI0IJKbbsbk/s1600/1.jpg",
     scores: [1, 3, 4, 3, 7, 2, 5, 1, 3, 5],
     card:
-      "<div class='card-body'><h5 class='card-title'>General Assembly</h5><h6 class='card-subtitle mb-2 text-muted'><img src='./images/general.png' alt='schoollogo'></h6><p class='card-text'>Phone: (303) 963-9936</p><a href=' https://generalassemb.ly/coding' target='_blank' class='card-link'>Learn More</a></div></div>"
+      "<div class='card-body'><h2 class='card-title'>General Assembly</h2><h6 class='card-subtitle mb-2 text-muted'><img src='./images/general.png' alt='schoollogo'></h6><p class='card-text'>Phone: (303) 963-9936</p><a href=' https://generalassemb.ly/coding' target='_blank' class='card-link'>Learn More</a></div></div>"
   },
   {
     name: "DENVER UNIVERSITY",
@@ -37,7 +37,7 @@ var courses = [
       "http://1.bp.blogspot.com/-dNvgygUycDQ/Tq8p4SUphpI/AAAAAAAABjo/UI0IJKbbsbk/s1600/1.jpg",
     scores: [1, 3, 4, 1, 7, 2, 2, 1, 4, 5],
     card:
-      "<div class='card-body'><h5 class='card-title'>Denver University</h5><h6 class='card-subtitle mb-2 text-muted'><img src='./images/DU.png' alt='schoollogo'></h6><p class='card-text'>Phone: (303) 223-1868</p><a href=' https://bootcamp.du.edu/coding/' target='_blank' class='card-link'>Learn More</a></div>"
+      "<div class='card-body'><h2 class='card-title'>Denver University</h2><h6 class='card-subtitle mb-2 text-muted'><img src='./images/DU.png' alt='schoollogo'></h6><p class='card-text'>Phone: (303) 223-1868</p><a href=' https://bootcamp.du.edu/coding/' target='_blank' class='card-link'>Learn More</a></div>"
   }
 ];
 
